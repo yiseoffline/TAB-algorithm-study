7ee200925a255e9f6a5ec692e844e72b5df6d313
yiseoffline <lsy0476@hufs.ac.kr>
2024-04-30 16:32:56 +0900

    16499 more short

--- 16499.py
+++ 16499.py
@@ -2,9 +2,23 @@
 
 n=int(input())
 arr=[]
+s = set()
+
+for i in range(n):
+    word = tuple(sorted(input()))
+    s.add(word)
+    
+print(len(s))
+
+'''
+# 16499
+
+n=int(input())
+arr=[]
 
 for i in range(n):
     word = sorted(list(input()))
     if word not in arr:
         arr.append(word)
 print(len(arr))
+'''
